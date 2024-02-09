f9f1f445e0490357d7a8b47ea20f28a98dc99d76
Ali Hashemi <am.hashemi97@gmail.com>
2024-02-10 01:36:57 +0330

    Changes

--- install-myblog.sh
+++ install-myblog.sh
@@ -88,21 +88,8 @@ add_node() {
 
 }
 
-#echo "Select Operation :"
-#echo "1. Add Node"
-#echo "2. Update Node"
-#echo "3. Backup"
-#read -r -p "Select Number(Default is: 1):" COMMAND
-#
-#case $COMMAND in
-#    1)  add_node
-#	  echo
-#    echo "=== Finished ==="
-#    echo
-#    exit ;;
-#
-#    *)   echo "Done."; exit 1 ;;
-
 add_node
+
+
 
 esac
