dc339b7dd694dd62d2ae63ed54b0d4f8fcc2488f
Ali Hashemi <am.hashemi97@gmail.com>
2024-02-10 01:03:24 +0330

    Changes

--- install-myblog.sh
+++ install-myblog.sh
@@ -4,34 +4,32 @@ COMMAND=1
 
 add_node() {
 
-  sudo apt update && sudo apt upgrade -y;
-
-  sudo bash -c "$(curl -sL https://github.com/Gozargah/Marzban-scripts/raw/master/marzban.sh)" @ install;
+#  sudo apt update && sudo apt upgrade -y;
+#
+#  sudo bash -c "$(curl -sL https://github.com/Gozargah/Marzban-scripts/raw/master/marzban.sh)" @ install;
 
   marzban down;
-#
-#  rm -r opt/marzban;
-#  mkdir opt/marzban;
-#
-#  rm -r var/lib/marzban/certs;
-#  rm var/lib/marzban/xray_config.json;
-#  rm var/lib/marzban/db.sqlite3;
-#
-#  mkdir var/lib/marzban/certs;
-#
-#  curl -o opt/marzban/.env https://github.com/Ali-Hashemi/My-Marzban-Script/blob/main/myblog-backup/opt/.env;
-#
-#  curl -o opt/marzban/docker-compose.yml https://github.com/Ali-Hashemi/My-Marzban-Script/blob/main/myblog-backup/opt/docker-compose.yml;
-#
-#  curl -o var/lib/marzban/db.sqlite3 https://github.com/Ali-Hashemi/My-Marzban-Script/blob/main/myblog-backup/var/db.sqlite3;
-#
-#  curl -o var/lib/marzban/xray_config.json https://github.com/Ali-Hashemi/My-Marzban-Script/blob/main/myblog-backup/var/xray_config.json;
-#
-#  mkdir var/lib/marzban/certs;
-#
-#  curl -o var/lib/marzban/certs/fullchain.pem https://github.com/Ali-Hashemi/My-Marzban-Script/blob/main/myblog-backup/var/certs/fullchain.pem;
-#
-#  curl -o var/lib/marzban/certs/key.pem https://github.com/Ali-Hashemi/My-Marzban-Script/blob/main/myblog-backup/var/certs/key.pem;
+
+  rm -r opt/marzban;
+  mkdir opt/marzban;
+
+  rm -r var/lib/marzban/certs;
+  rm var/lib/marzban/xray_config.json;
+  rm var/lib/marzban/db.sqlite3;
+
+  mkdir var/lib/marzban/certs;
+
+  curl -o opt/marzban/.env https://github.com/Ali-Hashemi/My-Marzban-Script/blob/main/myblog-backup/opt/.env;
+
+  curl -o opt/marzban/docker-compose.yml https://github.com/Ali-Hashemi/My-Marzban-Script/blob/main/myblog-backup/opt/docker-compose.yml;
+
+  curl -o var/lib/marzban/db.sqlite3 https://github.com/Ali-Hashemi/My-Marzban-Script/blob/main/myblog-backup/var/db.sqlite3;
+
+  curl -o var/lib/marzban/xray_config.json https://github.com/Ali-Hashemi/My-Marzban-Script/blob/main/myblog-backup/var/xray_config.json;
+
+  curl -o var/lib/marzban/certs/fullchain.pem https://github.com/Ali-Hashemi/My-Marzban-Script/blob/main/myblog-backup/var/certs/fullchain.pem;
+
+  curl -o var/lib/marzban/certs/key.pem https://github.com/Ali-Hashemi/My-Marzban-Script/blob/main/myblog-backup/var/certs/key.pem;
 
 }
 
