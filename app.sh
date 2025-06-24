da124d173837282bde608dc1d18d56ca80cdb7b2
amazon98098 <amazon98098@gmail.com>
2025-06-24 14:50:23 +0330

    Update app.sh

--- app.sh
+++ app.sh
@@ -298,13 +298,13 @@ telegram_proxy(){
 }
 
 backhaul_tunnel(){
-	apt install python3 -y && sudo apt install python3-pip &&  pip install colorama && pip install netifaces && apt install curl -y
+	# apt install python3 -y && sudo apt install python3-pip &&  pip install colorama && pip install netifaces && apt install curl -y
 	
-	pip3 install colorama
+	# pip3 install colorama
 	
-	sudo apt-get install python-pip -y  &&  apt-get install python3 -y && alias python=python3 && python -m pip install colorama && python -m pip install netifaces
+	# sudo apt-get install python-pip -y  &&  apt-get install python3 -y && alias python=python3 && python -m pip install colorama && python -m pip install netifaces
 	
-	sudo apt update -y && sudo apt install -y python3 python3-pip curl && pip3 install --upgrade pip && pip3 install netifaces colorama requests
+	# sudo apt update -y && sudo apt install -y python3 python3-pip curl && pip3 install --upgrade pip && pip3 install netifaces colorama requests
 	
 	# bash -c "$(curl -fsSL https://raw.githubusercontent.com/Azumi67/Backhaul_script/refs/heads/main/backhaul.sh)" 
 
